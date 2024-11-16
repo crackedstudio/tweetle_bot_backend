c3ebd67273ebeddad7c370e13eec4c8a9cbe8c68
manoahLinks <manoahluka@gmail.com>
2024-11-16 12:49:24 +0100

    feat: impl game logic

--- index.js
+++ index.js
@@ -1,23 +1,72 @@
 require('dotenv').config()
 const express = require('express'),
-    app = express();
+    app = express(),
+    TelegramBot = require('node-telegram-bot-api'),
+    bot = new TelegramBot(process.env.BOT_API_KEY, {polling: true});
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 
-app.get('/', async (req, res) => {
+// Matches "/echo [whatever]"
+bot.onText(/\/echo(.+)/, (msg, match) => {
 
-    try {
+    // The 'msg' is the received Message from Telegram
+    // and 'match' is the result of executing the regexp 
+    // above on the text content of the message
 
-        const response = await fetch('https://api.telegram.org/bot7550266948:AAF5oobYdr9eZHmhC0hkI9ldXqzKBgoIfGo/getUpdates')
+    let chatId = msg.chat.id;
 
-        res.json({data: await response.json()})
+    // The captured "whatever"
+    let resp = match[1];
 
-    } catch (error) {
-        res.json({message: 'error', error: error.message})
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const messageText = msg.text;
+    let result = [];
+    let emojiArray = []
+
+    let wordOfTheDay = ['C', 'o', 'v', 'i', 'd']
+    let msgArray = messageText.split('')
+
+    console.log(wordOfTheDay, msgArray)
+
+    if (messageText.length != 5) {
+        return bot.sendMessage(chatId, 'You reply must be a 5 letter word')
     }
-})
+
+    for (let i = 0; i < msgArray.length; i++) {
+        const letter = msgArray[i];
+        const isValid = wordOfTheDay.includes(letter); // Check if the letter exists in arr1
+        const isRight = wordOfTheDay[i] === letter; // Check if the letter is at the same index in both arrays
+    
+        // Add the letter and its validation status as an object to the result array
+        result.push({
+          [letter]: {
+            isValid,
+            isRight
+          }
+        });
+
+        if (isValid && isRight) {
+            emojiArray.push('🟩')
+        }else if(!isRight && isValid) {
+            emojiArray.push('🟧')
+        }else if(!isValid && !isRight) {
+            emojiArray.push('⬛')
+        }
+      }
+    
+    console.log(result);
+    console.log(emojiArray);
+
+    return bot.sendMessage(chatId, `${emojiArray.join('')}`)
+  });
+
 
 app.listen(process.env.PORT, () => {
     console.log("bot backend started @", process.env.PORT)
