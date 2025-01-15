97a255b5a2b60076b22be32d7b6b128ae3369db3
manoahLinks <manoahluka@gmail.com>
2025-01-16 00:12:26 +0100

    chore: switch to atlas

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.LOCAL_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CLOUD_URI, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=>{
         console.log('DB connected')
         console.log('-----------------------------------------')
